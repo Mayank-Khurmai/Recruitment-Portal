ee34e3bebba438a2ea460eacaaee1873e1ec8964
Mayank Khurmai <mayankkhurmai8@gmail.com>
2020-06-17 00:40:49 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -9,9 +9,3 @@ function afterloader(){
     document.getElementById("full-body").style.display = "block";
 }
 
-function edit_student(){
-    $('.edit-stu').click(function(){
-        var edit_id = $(this).attr("data");
-        $('[login_id="+edit_id+"]').attr("contenteditable", true);
-    });
-}
