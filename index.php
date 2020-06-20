344c94f44b18c18afb0eb2db27f6042e6bba799d
Mayank Khurmai <mayankkhurmai8@gmail.com>
2020-06-20 12:21:33 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -9,7 +9,6 @@
 
     <script src="js/jquery-1.9.1.min.js"></script>
     <script src="js/main.js"></script>
-	<script></script>
 </head>
 <body onload="onbodyload()">
   
@@ -49,112 +48,6 @@
                         <input type="password" id="input-pass" placeholder="Password">
                     </div>
                 </div>
-                //function Start
-				function view_result($dmail)
-				{
-					$this->query = "SELECT * FROM exam_result, question_answer WHERE exam_result.ques_id = question_answer.ques_id AND email='mayankkhurmai8@gmail.com'";
-					$this->response = $this->db->query($this->query);
-					if($this->response->num_rows != 0)
-					{ 
-						$total=0;
-						echo "<table border='1px' cellspacing='1px' cellpadding='4px' style='width:80%;'>";
-						echo "<tr style='background-color:yellow'><th width='10%'>Student No.</th><th width='25%'>Email</th><th width='8%'>Ques ID</th><th>A</th><th>B</th><th>C</th><th>D</th><th width='8%'>Weightage</th></tr>";
-						while($data = $this->response->fetch_assoc())
-						{
-							$x=0;
-							$a="background-color:red";
-							$b="background-color:red";
-							$c="background-color:red";
-							$d="background-color:red";
-							if($data['marked']==1)
-							{
-								if($data['is_radiobox']==1)
-								{
-									if($data['option_a_ans']==$data['option_a_final_ans'])
-									{
-										$a = "background-color:green";
-									}
-									if($data['option_b_ans']==$data['option_b_final_ans'])
-									{
-										$b = "background-color:green";
-									}
-									if($data['option_c_ans']==$data['option_c_final_ans'])
-									{
-										$c = "background-color:green";
-									}
-									if($data['option_d_ans']==$data['option_d_final_ans'])
-									{
-										$d = "background-color:green";
-										if($data['option_c_ans']==$data['option_c_final_ans'])
-										{
-											if($data['option_b_ans']==$data['option_b_final_ans'])
-											{
-												if($data['option_a_ans']==$data['option_a_final_ans'])
-												{
-													$x=1;
-												}
-											}
-										}
-									}			
-								} 
-								else
-								{
-									if($data['option_a_ans']==$data['option_a_final_ans'])
-									{
-										$a = "background-color:green";
-										$x+=0.25;
-									}
-									if($data['option_b_ans']==$data['option_b_final_ans'])
-									{
-										$b = "background-color:green";
-										$x+=0.25;
-									}
-									if($data['option_c_ans']==$data['option_c_final_ans'])
-									{
-										$c = "background-color:green";
-										$x+=0.25;
-									}
-									if($data['option_d_ans']==$data['option_d_final_ans'])
-									{
-										$d = "background-color:green";
-										$x+=0.25;
-									}
-								}
-							}
-							$total+=$x;
-							echo "
-								<tr>
-									<td style='padding-left:5px'>".$data['student_no']."</td>
-									<td style='padding-left:5px'>".$data['email']."</td>
-									<th style='padding-left:5px'>".$data['ques_id']."</th>
-									<th style='padding-left:5px; ".$a."'>".$data['option_a_final_ans']."</th>
-									<th style='padding-left:5px; ".$b."''>".$data['option_b_final_ans']."</th>
-									<th style='padding-left:5px; ".$c."''>".$data['option_c_final_ans']."</th>
-									<th style='padding-left:5px; ".$d."''>".$data['option_d_final_ans']."</th>
-									<th style='padding-left:5px;'>".$x."</th>
-								</tr>
-								";
-						}
-						echo "</table>";
-						echo $total;
-					}
-					else
-					{
-						// echo "<table border='1px' cellspacing='1px' cellpadding='4px' style='width:100%;'>
-						// 		<tr style='background-color:yellow'><th width='10%'>Student No.</th><th width='25%'>Email</th><th width='8%'>Ques ID</th><th width='5%'>Visited</th><th width='5%'>Attempted</th><th width='5%'>Reviewed</th><th width='10%'>Multi Answer</th><th>A</th><th>B</th><th>C</th><th>D</th><th width='8%'>Weightage</th></tr>
-						// 		<tr><th colspan='12'>No Data Found</th></tr>
-						// 		</table>
-						// 	";	
-					}
-				}
-				while($data = $this->response_a->fetch_assoc())
-				{
-					$dmail = $data['email'];
-					view_result($dmail);
-
-				}
-			
-				//Function End
                 <br><br>
                 <div class="submit-div">
                     <button type="submit" id="submit-div-btn">Login</button>
